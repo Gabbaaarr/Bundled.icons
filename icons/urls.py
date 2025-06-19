1efebcd2696757718a4060c500fc2345045ed76b
Vinayak Vispute <vinayakvispute4@gmail.com>
2025-06-19 13:40:43 +0530

    fix:home page redirection issue

--- icons/urls.py
+++ icons/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('svgiconsfree', views.home, name='home'),
+    path('svgicons', views.home, name='home'),
     path('download/', views.download_icon, name='download_icon'),
     path('icons/<slug:category_slug>/', views.category_icons, name='category_icons'),
 ]
